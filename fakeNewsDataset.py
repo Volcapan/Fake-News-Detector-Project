1004b6380f2b7f06bb697313f7db86e2a3ac8737
Jeffrey Xiong <jeffx299@gmail.com>
2025-07-05 02:34:53 -0700

    Did more work on fake news dataset.

--- fakeNewsDataset.py
+++ fakeNewsDataset.py
@@ -3,4 +3,12 @@ from torch.utils.data import Dataset
 
 class fakeNewsDataset(Dataset):
     def __init__(self, news):
-        print()
+        self.samples = news.drop("truthfulness", axis=1)
+        self.labels = news["truthfulness"]
+        self.len = len(self.news)
+
+    def __len__(self):
+        return self.len
+    
+    def __getitem__(self, index):
+        return self.samples[index], self.labels[index]
